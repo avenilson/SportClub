034cc9a41014cfb2e3fd5b21c66e09162d9762c7
Mariliis Sinivee <masini@ttu.ee>
2020-04-17 19:40:04 +0300

    Migratsioonide kaust olemas

--- Soft/Migrations/20200417163800_initial.cs
+++ Soft/Migrations/20200417163800_initial.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace Soft.Data.Migrations
+namespace SportClub.Soft.Migrations
 {
     public partial class initial : Migration
     {
--- Soft/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Soft/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -3,17 +3,15 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportClub.Soft.Data;
 
-namespace Soft.Data.Migrations
+namespace SportClub.Soft.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20200412105025_init")]
-    partial class init
+    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
