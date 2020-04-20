3275b6f41de59eb539ad4c21c8a595db948e36ad
Helen Kalamäe <hekala@ttu.ee>
2020-04-20 16:40:22 +0300

    domain

---
+++ Domain/CoachOfTraining/CoachOfTraining.cs
@@ -0,0 +1,13 @@
+﻿using SportClub.Data.TrainingCoach;
+using SportClub.Domain.Common;
+
+namespace SportClub.Domain.CoachOfTraining
+{
+    public sealed class CoachOfTraining : Entity<CoachOfTrainingData>
+    {
+        public CoachOfTraining() : this(null) { }
+        public CoachOfTraining(TrainingCoachData data) : base(data) { }
+
+        
+    }
+}
