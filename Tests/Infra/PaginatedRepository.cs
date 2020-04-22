f6834241135952b6b01e479ffeb29b73c2257dad
Ave Nilson <avnils@ttu.ee>
2020-04-22 12:31:49 +0300

    InfraTests

---
+++ Tests/Infra/PaginatedRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SportClub.Data.Common;
+using SportClub.Domain.Common;
+
+namespace SportClub.Tests.Infra
+{
+    public abstract class PaginatedRepository<TDomain, TData>:FilteredRepository<TDomain, TData>, IPaging
+        where TData : NamedEntityData, new()
+        where TDomain : Entity<TData>, new()
+    {
+        public int PageIndex { get; set; }
+        public int TotalPages => GetTotalPages(PageSize);
+        public bool HasNextPage => PageIndex < TotalPages;
+        public bool HasPreviousPage => PageIndex > 1;
+        public int PageSize { get; set; } = 5;
+
+        protected PaginatedRepository(DbContext c, DbSet<TData> s) : base(c, s) { }
+        
+        internal int GetTotalPages(in int pageSize)
+        {
+            var count = GetItemsCount();
+            var pages = CountTotalPages(count, pageSize);
+            return pages;
+        }
+
+        internal int CountTotalPages(int count, in int pageSize)=> (int)Math.Ceiling(count / (double)pageSize);
+
+        internal int GetItemsCount()=> base.createSqlQuery().CountAsync().Result;
+
+        protected internal override IQueryable<TData> createSqlQuery()=> AddSkipAndTake(base.createSqlQuery());
+
+        internal IQueryable<TData> AddSkipAndTake(IQueryable<TData> query)
+        {
+            if (PageIndex < 1) return query;
+            return query
+                .Skip((PageIndex - 1) * PageSize)
+                .Take(PageSize);
+        } 
+    }
+}
---
+++ Tests/Infra/UniqueEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SportClub.Data.Common;
+using SportClub.Domain.Common;
+
+namespace SportClub.Tests.Infra
+{
+    public abstract class UniqueEntityRepository<TDomain, TData>:PaginatedRepository<TDomain, TData>
+    where TData: NamedEntityData, new()
+    where TDomain: Entity<TData>, new()
+    {
+        protected UniqueEntityRepository(DbContext c, DbSet<TData> s) : base(c, s) { }
+        protected override async Task<TData> getData(string id)
+            => await dbSet.FirstOrDefaultAsync(m => m.Id == id);
+
+        protected override string getId(TDomain entity) => entity?.Data?.Id;
+    }
+}
