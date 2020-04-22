2a1ff367d4e843e63b9e009995a0c261fc1949a0
Helen Kalamäe <hekala@ttu.ee>
2020-04-22 09:11:06 +0300

    unique ja paginated infra

--- Infra/PaginatedRepository.cs
+++ Infra/PaginatedRepository.cs
@@ -1,10 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Microsoft.EntityFrameworkCore;
+using SportClub.Domain.Common;
 
 namespace SportClub.Infra
 {
-    class PaginatedRepository
+    public abstract class PaginatedRepository<TDomain, TData>: FilteredRepository<TDomain, TData>, IPaging
+        where TData: PeriodData, new()
+        where TDomain: Entity<TData>, new()
     {
+        public int PageIndex { get; set; }
+        public int TotalPages => getTotalPages(PageSize);
+        public bool HasNextPage => PageIndex < TotalPages; //lk indeks peab olema vaiksem kui lk arv, siis on veel jargm lk olemas
+        public bool HasPreviousPage => PageIndex > 1; //siis on eelm lk olemas
+        public int PageSize { get; set; } = 5; //5 kirjet lkl
+
+        protected PaginatedRepository(DbContext c, DbSet<TData> s) : base(c, s) { }
+
+        internal int getTotalPages(in int pageSize)
+        {
+            var count = getItemsCount(); //palju on kirjeid
+            var pages = countTotalPages(count, pageSize);
+
+            return pages;
+        }
+
+        internal int countTotalPages(int count, in int pageSize) //internal on nagu private, aga saab testida. keegi valjast ligi ei saa
+        {
+            return (int)Math.Ceiling(count / (double)pageSize);
+        }
+
+        internal int getItemsCount() => base.createSqlQuery().CountAsync().Result; //result ehk saame asunkr meetodit kutsuda valja sunkr meetodis
+        
+        protected internal override IQueryable<TData> createSqlQuery() => addSkipAndTake(base.createSqlQuery()); //lisab skipi (see oli paginatedlist kirjas)
+
+        internal IQueryable<TData> addSkipAndTake(IQueryable<TData> query)
+        {
+            if (PageIndex < 1) return query;
+            return query
+                .Skip((PageIndex - 1) * PageSize)
+                .Take(PageSize);
+        }
     }
 }
--- Infra/UniqueEntityRepository.cs
+++ Infra/UniqueEntityRepository.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SportClub.Data.Common;
+using SportClub.Domain.Common;
 
 namespace SportClub.Infra
 {
-    class UniqueEntityRepository
+    public abstract class UniqueEntityRepository<TDomain, TData>: PaginatedRepository<TDomain, TData>
+        where TData: UniqueEntityData, new()
+        where TDomain: Entity<TData>, new()
     {
+        protected UniqueEntityRepository(DbContext c, DbSet<TData> s) : base(c, s) { }
+        
+        protected override async Task<TData> getData(string id)
+            => await dbSet.FirstOrDefaultAsync(m => m.Id == id);
+
+        protected override string getId(TDomain entity) => entity?.Data?.Id;
     }
 }
