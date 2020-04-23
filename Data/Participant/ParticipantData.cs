ab30d69794f8f1f1574e888dfa1b1f7db7dc64b7
Ave Nilson <avnils@ttu.ee>
2020-04-23 10:33:00 +0300

    Testid

--- Data/Participant/ParticipantData.cs
+++ Data/Participant/ParticipantData.cs
@@ -4,7 +4,7 @@ namespace SportClub.Data.Participant
 {
     public sealed class ParticipantData: NamedEntityData
     {
-        public string Age { get; set; }
+        public int Age { get; set; }
         public string Address { get; set; }
     }
 }
