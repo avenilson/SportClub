bb1f9226b0136588aacc955b97a325ab0deb8840
Ave Nilson <avnils@ttu.ee>
2020-04-26 10:21:57 +0300

    InfraTests / Training

--- Tests/Infra/Training/TrainingsRepositoryTests.cs
+++ Tests/Infra/Training/TrainingsRepositoryTests.cs
@@ -1,10 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportClub.Data.Training;
+using SportClub.Infra;
+using SportClub.Infra.Training;
 
 namespace SportClub.Tests.Infra.Training
 {
-    class TrainingsRepositoryTests
+
+    [TestClass]
+    public class TrainingsRepositoryTests : RepositoryTests<TrainingsRepository, SportClub.Domain.Training.Training, TrainingData>
     {
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            var options = new DbContextOptionsBuilder<SportClubDbContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+            db = new SportClubDbContext(options);
+            dbSet = ((SportClubDbContext)db).Trainings;
+            obj = new TrainingsRepository((SportClubDbContext)db);
+            base.TestInitialize();
+        }
+
+        protected override Type GetBaseType() => typeof(UniqueEntityRepository<SportClub.Domain.Training.Training, TrainingData>);
+
+        protected override string GetId(TrainingData d) => d.Id;
+
+        protected override SportClub.Domain.Training.Training GetObject(TrainingData d) => new SportClub.Domain.Training.Training(d);
+
+        protected override void SetId(TrainingData d, string id) => d.Id = id;
     }
 }
