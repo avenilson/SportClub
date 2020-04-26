21b3a655dda7c19e819a97e23d6ba65b35a3a99c
Ave Nilson <avnils@ttu.ee>
2020-04-26 10:10:06 +0300

    InfraTests / CoachesOfTrainings ja Participants

--- Tests/Infra/CoachOfTraining/CoachesOfTrainingsRepositoryTests.cs
+++ Tests/Infra/CoachOfTraining/CoachesOfTrainingsRepositoryTests.cs
@@ -1,10 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportClub.Aids;
+using SportClub.Data.CoachOfTraining;
+using SportClub.Infra;
+using SportClub.Infra.CoachOfTraining;
 
 namespace SportClub.Tests.Infra.CoachOfTraining
 {
-    class CoachesOfTrainingsRepositoryTests
+
+    [TestClass]
+    public class CoachesOfTrainingsRepositoryTests : RepositoryTests<CoachOfTrainingsRepository, SportClub.Domain.CoachOfTraining.CoachOfTraining, CoachOfTrainingData>
     {
+        [TestInitialize]
+        public override void TestInitialize()
+        {
+            var options = new DbContextOptionsBuilder<SportClubDbContext>()
+                .UseInMemoryDatabase("TestDb")
+                .Options;
+            db = new SportClubDbContext(options);
+            dbSet = ((SportClubDbContext)db).CoachesOfTrainings;
+            obj = new CoachOfTrainingsRepository((SportClubDbContext)db);
+            base.TestInitialize();
+        }
+
+        protected override Type GetBaseType() => typeof(PaginatedRepository<SportClub.Domain.CoachOfTraining.CoachOfTraining, CoachOfTrainingData>);
+
+        protected override string GetId(CoachOfTrainingData d) => $"{d.CoachId}.{d.TrainingId}";
+
+        protected override SportClub.Domain.CoachOfTraining.CoachOfTraining GetObject(CoachOfTrainingData d) => new SportClub.Domain.CoachOfTraining.CoachOfTraining(d);
+
+        protected override void SetId(CoachOfTrainingData d, string id)
+        {
+            var coachId = GetString.Head(id);
+            var trainingId = GetString.Tail(id);
+            d.CoachId = coachId;
+            d.TrainingId = trainingId;
+        }
     }
 }
