6c7701670d4f365258b08e3ef7df8a5830015b7a
Mariliis Sinivee <masini@ttu.ee>
2020-04-28 14:36:14 +0300

    Coach vaatel filter, sort, search olemas

--- Soft/Areas/Coach/Pages/Coaches/Index.cshtml.cs
+++ Soft/Areas/Coach/Pages/Coaches/Index.cshtml.cs
@@ -1,29 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using SportClub.Data.Coach;
-using SportClub.Infra;
+using SportClub.Domain.Coach;
+using SportClub.Pages.Coach;
 
 namespace SportClub.Soft.Areas.Coach.Pages.Coaches
 {
-    public class IndexModel : PageModel
+    public class IndexModel : CoachesPage
     {
-        private readonly SportClub.Infra.SportClubDbContext _context;
-
-        public IndexModel(SportClub.Infra.SportClubDbContext context)
+        public IndexModel(ICoachesRepository r) : base(r)
         {
-            _context = context;
         }
 
         public IList<CoachData> CoachData { get;set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder,
+            string id, string currentFilter, string searchString, int? pageIndex,
+            string fixedFilter, string fixedValue)
         {
-            CoachData = await _context.Coaches.ToListAsync();
+            SelectedId = id;
+            await GetList(sortOrder, currentFilter, searchString, pageIndex,
+                fixedFilter, fixedValue);
+
         }
     }
 }
