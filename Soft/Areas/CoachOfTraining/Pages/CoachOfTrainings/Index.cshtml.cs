719a06d2ba2496300414b4503b20a2c43bd2965e
Mariliis Sinivee <masini@ttu.ee>
2020-04-28 14:44:26 +0300

    CoachOfTraining vaatel filter, sort ja search olemas

--- Soft/Areas/CoachOfTraining/Pages/CoachOfTrainings/Index.cshtml.cs
+++ Soft/Areas/CoachOfTraining/Pages/CoachOfTrainings/Index.cshtml.cs
@@ -5,25 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using SportClub.Data.Coach;
 using SportClub.Data.CoachOfTraining;
+using SportClub.Domain.Coach;
+using SportClub.Domain.CoachOfTraining;
 using SportClub.Infra;
+using SportClub.Pages.CoachOfTraining;
 
 namespace SportClub.Soft.Areas.CoachOfTraining.Pages.CoachOfTrainings
 {
-    public class IndexModel : PageModel
+    public class IndexModel : CoachOfTrainingsPage
     {
-        private readonly SportClub.Infra.SportClubDbContext _context;
-
-        public IndexModel(SportClub.Infra.SportClubDbContext context)
+        public IndexModel(ICoachOfTrainingsRepository r) : base(r)
         {
-            _context = context;
         }
 
-        public IList<CoachOfTrainingData> CoachOfTrainingData { get;set; }
+        public IList<CoachOfTrainingData> CoachOfTrainingData { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder,
+            string id, string currentFilter, string searchString, int? pageIndex,
+            string fixedFilter, string fixedValue)
         {
-            CoachOfTrainingData = await _context.CoachesOfTrainings.ToListAsync();
+            SelectedId = id;
+            await GetList(sortOrder, currentFilter, searchString, pageIndex,
+                fixedFilter, fixedValue);
+
         }
     }
 }
