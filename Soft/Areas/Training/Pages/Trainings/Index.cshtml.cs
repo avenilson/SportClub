5d477c959da2929dd1bcda10d29b7b503ecd6d6e
Mariliis Sinivee <masini@ttu.ee>
2020-04-28 15:12:41 +0300

    Training vaatel sort, filter ja search olemas

--- Soft/Areas/Training/Pages/Trainings/Index.cshtml.cs
+++ Soft/Areas/Training/Pages/Trainings/Index.cshtml.cs
@@ -5,25 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using SportClub.Data.ParticipantOfTraining;
 using SportClub.Data.Training;
+using SportClub.Domain.Training;
 using SportClub.Infra;
+using SportClub.Pages.Training;
 
 namespace SportClub.Soft.Areas.Training.Pages.Trainings
 {
-    public class IndexModel : PageModel
+    public class IndexModel : TrainingsPage
     {
-        private readonly SportClub.Infra.SportClubDbContext _context;
-
-        public IndexModel(SportClub.Infra.SportClubDbContext context)
+        public IndexModel(ITrainingsRepository r) : base(r)
         {
-            _context = context;
         }
 
-        public IList<TrainingData> TrainingData { get;set; }
+        public IList<TrainingData> TrainingData { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder,
+            string id, string currentFilter, string searchString, int? pageIndex,
+            string fixedFilter, string fixedValue)
         {
-            TrainingData = await _context.Trainings.ToListAsync();
+            SelectedId = id;
+            await GetList(sortOrder, currentFilter, searchString, pageIndex,
+                fixedFilter, fixedValue);
+
         }
     }
 }
