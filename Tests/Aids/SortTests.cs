c6bf7406a90da6f06c90908adbb39a4821940e38
Ave Nilson <avnils@ttu.ee>
2020-05-14 11:28:13 +0300

    Aids / SortTests

--- Tests/Aids/SortTests.cs
+++ Tests/Aids/SortTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SportClub.Aids;
 
 namespace SportClub.Tests.Aids {
@@ -11,37 +12,12 @@ namespace SportClub.Tests.Aids {
         [TestMethod]
         public void UpwardsTest()
         {
-           Assert.Inconclusive();
+            var random = new Random();
+            int min = random.Next(); 
+            int max = random.Next();
+            TestArePropertyValuesEqual(min, max);
         }
-
-        //[TestMethod] public void AscendingTest() {
-        //    sortAscendingTest(DateTime.MaxValue, DateTime.MinValue);
-        //    sortAscendingTest(double.MaxValue, double.MinValue);
-        //    sortAscendingTest(int.MaxValue, int.MinValue);
-        //}
-
-        //private static void sortAscendingTest<T>(T max, T min) where T : IComparable {
-        //    Assert.IsTrue(max.CompareTo(min) >= 0);
-        //    Sort.Ascending(ref max, ref min);
-        //    Assert.IsTrue(max.CompareTo(min) <= 0);
-        //}
-        //[TestMethod]
-        //public void DescendingTest()
-        //{
-        //    sortDescendingTest(DateTime.MinValue, DateTime.MaxValue);
-        //    sortDescendingTest(double.MinValue, double.MaxValue);
-        //    sortDescendingTest(int.MinValue, int.MaxValue);
-        //}
-
-        //private static void sortDescendingTest<T>(T min, T max) where T : IComparable
-        //{
-        //    Assert.IsTrue(min.CompareTo(max) <= 0);
-        //    Sort.Descending(ref min, ref max);
-        //    Assert.IsTrue(min.CompareTo(max) >= 0);
-        //}
-
     }
-
 }
 
 
