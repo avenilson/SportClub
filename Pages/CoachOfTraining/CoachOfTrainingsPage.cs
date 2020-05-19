af1a268abd3e8681fd65ca664f6c2eea14ade8c6
Mariliis Sinivee <masini@ttu.ee>
2020-05-19 08:41:30 +0300

    Vaadetes stiili parandused ja dropdown listi loomine

--- Pages/CoachOfTraining/CoachOfTrainingsPage.cs
+++ Pages/CoachOfTraining/CoachOfTrainingsPage.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using SportClub.Data.Coach;
 using SportClub.Data.CoachOfTraining;
@@ -18,6 +19,7 @@ namespace SportClub.Pages.CoachOfTraining
             CoachId= CreateSelectList2<Domain.Coach.Coach, CoachData>(c);
             TrainingId = CreateSelectList<Domain.Training.Training, TrainingData>(t);
             Ids = CreateSelectList3<Domain.Coach.Coach, CoachData>(c);
+            ViewData["sample"] = Items.Select(x => new SelectListItem { Text = Item.CoachId, Value = Item.Id });
         }
         public IEnumerable<SelectListItem> CoachId { get; }
         public IEnumerable<SelectListItem> TrainingId { get; }
